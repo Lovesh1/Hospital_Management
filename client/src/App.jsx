f9f00daff485d40d32fd03f977e19f50f870de18
ITMsJarvis <ck8824deb@gmail.com>
2023-10-25 22:20:57 +0530

    Fix/

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Login from './Pages/LoginPage.jsx'
-import Dashboard from './Pages/DashboardPage.jsx'
-import Patients from "./Pages/Patients.jsx"
+import Layout from "./Pages/Layout.jsx"
 
 export default function App() {
   return (
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/patient" element={<Patients />} />
+          <Route path="/dashboard" element={<Layout />} />
         </Routes>
       </Router>
   )
---
+++ client/src/Pages/Nurse.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Nurse() {
+  return (
+    <div>
+      <h1 className="text-3xl text-slate-500">Add one more field in model.nurse so that total fields would be 18 i added the 16 field right now... </h1>
+    </div>
+  )
+}
